891e6d0cdd5dc6c0beace8208fd6c181d61f5a32
ncthompson <github@hoekwil.com>
2017-03-05 15:57:36 +0200

    Add repo to ubuntu.

---
+++ scrips/install_influx_repo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
